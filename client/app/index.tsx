96955d0b15c52f045683a362c53856c31b812323
arijitchakma79 <arijitchakma79@gmail.com>
2025-04-26 15:26:09 -0400

    added basic app ui

---
+++ client/app/index.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { View, Text } from 'react-native';
+
+
+const Home = () =>  {
+  return (
+    <View
+      style={{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Text>Hello World</Text>
+      </View>
+  )
+}
+
+export default Home;
+  
