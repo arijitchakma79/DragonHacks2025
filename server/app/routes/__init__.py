1186507bab7673d3467e9fa32fbc79d552721e33
SafaObuz <61282410+SafaObuz@users.noreply.github.com>
2025-04-27 09:47:19 -0400

    segmentation with flask api

--- server/app/routes/__init__.py
+++ server/app/routes/__init__.py
@@ -1,9 +1,11 @@
 from .upload_wound import upload_wound_bp
 from .list_user_wounds import list_wounds_bp
 from .download_routes import download_bp
+from .measure_wound import measure_bp 
 
 all_blueprints = [
     upload_wound_bp,
     list_wounds_bp,
-    download_bp
+    download_bp,
+    measure_bp
 ]
